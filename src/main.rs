7263f42842faa6bf84d9f5574447bbc5da3fb9cd
Victor B. <contact@itsvic.dev>
2026-03-16 03:57:05 +0100

    start stupid thing

---
+++ src/main.rs
@@ -0,0 +1,25 @@
+#![no_std]
+#![no_main]
+
+use core::{panic::PanicInfo, ptr::write_volatile};
+
+mod uart;
+
+#[unsafe(no_mangle)]
+fn _start() {
+    uart::uart_init();
+
+    println!("hello, world!");
+
+    // syscon shutdown
+    unsafe {
+        write_volatile(0x100000 as *mut u32, 0x5555);
+    }
+    loop {}
+}
+
+#[panic_handler]
+fn panic(_p: &PanicInfo) -> ! {
+    // FIXME
+    loop {}
+}
---
+++ src/uart.rs
@@ -0,0 +1,56 @@
+// hardcoded for now, needs to be read from DTB
+use core::ptr::write_volatile;
+
+const UART_ADDR: *mut u8 = 0x10000000 as *mut u8;
+
+pub fn uart_init() {
+    unsafe {
+        let line_control_reg = UART_ADDR.offset(3);
+        write_volatile(line_control_reg, 3); // 8-bit words
+        write_volatile(line_control_reg, 1); // enable FIFOs
+        write_volatile(UART_ADDR.offset(1), 1); // enable receiver interrupts
+    }
+}
+
+fn uart_putc(c: u8) {
+    unsafe {
+        write_volatile(UART_ADDR, c);
+    }
+}
+
+pub fn uart_puts(s: &str) {
+    for c in s.bytes() {
+        uart_putc(c);
+    }
+}
+
+pub struct Writer;
+impl core::fmt::Write for Writer {
+    fn write_str(&mut self, s: &str) -> core::fmt::Result {
+        uart_puts(s);
+        Ok(())
+    }
+}
+
+pub fn print(args: core::fmt::Arguments) {
+    use core::fmt::Write;
+    Writer {}.write_fmt(args).unwrap();
+}
+
+#[macro_export]
+macro_rules! print {
+    ($($arg:tt)*) => {{
+        $crate::uart::print(format_args!($($arg)*));
+    }}
+}
+
+#[macro_export]
+macro_rules! println {
+    () => {{
+        print!("\n");
+    }};
+
+    ($($arg:tt)*) => {{
+        print!("{}\n", format_args!($($arg)*));
+    }}
+}
